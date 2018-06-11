3b675b1e5bf355e77f31a4ce08d9928d4f78beb5
Eric Zell <ezell@rbmtechnologies.com>
2018-06-11 19:57:49 -0400

    Add a copy method

--- binary_tree_node.rb
+++ binary_tree_node.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require_relative 'queue'
+require_relative 'stack'
 class Node
   attr_accessor :value, :left_child, :right_child
   def initialize(value)
@@ -18,37 +19,48 @@ class Node
       [true, false].sample ? @left_child.insert(new_value) : @right_child.insert(new_value)
     end
   end
+
 end
 
 def inorder_traversal(node)
   if node
-    inorder_traversal(node.left_child, &block)
+    inorder_traversal(node.left_child)
     puts node.value
-    inorder_traversal(node.right_child, &block)
+    inorder_traversal(node.right_child)
   end
 end
 
 def preorder_traversal(node)
   if node
     puts node.value
-    preorder_traversal(node.left_child, &block)
-    preorder_traversal(node.right_child, &block)
+    preorder_traversal(node.left_child)
+    preorder_traversal(node.right_child)
   end
 end
 
 def postorder_traversal(node)
   if node
-    postorder_traversal(node.left_child, &block)
-    postorder_traversal(node.right_child, &block)
+    postorder_traversal(node.left_child)
+    postorder_traversal(node.right_child)
     puts node.value
   end
 end
 
 def depth_first_search(node)
   if node
-    puts node.value, search_item
-    depth_first_search(node.left_child, search_item, &block)
-    depth_first_search(node.right_child, search_item, &block)
+    puts node.value
+    depth_first_search(node.left_child)
+    depth_first_search(node.right_child)
+  end
+end
+
+def depth_first_iterative(node)
+  stack = Stack.new(node)
+  while !stack.is_empty?
+    node = stack.pop
+    puts node.value
+    stack.push(node.right_child) if node.right_child
+    stack.push(node.left_child) if node.left_child
   end
 end
 
@@ -62,5 +74,16 @@ def breadth_first_search(node)
   end
 end
 
+def copy(node)
+  if node
+    new_node = Node.new(node.value)
+    new_node.left_child = copy(node.left_child)
+    new_node.right_child = copy(node.right_child)
+    return new_node
+  end
+end
+
 root = Node.new(0)
 (1..20).each{ |i| root.insert(i) }
+binding.pry
+puts "exiting"
